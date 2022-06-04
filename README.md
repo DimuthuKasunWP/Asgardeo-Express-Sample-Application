8810325c71de69df223c950b57d3b2a415f2e822
DimuthuKasunWP <dimuthuk@wso2.com>
2022-06-04 10:01:29 +0530

    Adding Asgardeo SDK funtionalities

--- README.md
+++ README.md
@@ -1,13 +1,25 @@
 <p align="center">
   <img src="static/images/landingpage.png" alt="Screenshot">
 </p>
+<p align="center">
+  <img src="static/images/homepage.png" alt="Screenshot">
+</p>
 
 ## 🚀 Getting started
 
 Clone this repository and do the following inside the project folder:
 
-1. Run `npm i`
-2. Run `npm run serve` to start the server with nodeman.
+1. Enter the correct values for the below fields in config.json file.
+
+      ```
+      "clientID": "<client_id>",
+      "clientSecret": "<client_secret>",
+      "baseUrl": "https://api.asgardeo.io/t/<org_name>",
+      "appURL": "http://localhost:3000",
+      "scope": ["openid", "profile"]
+      ```
+2. Run `npm i`
+3. Run `npm run serve` to start the server with nodeman.
 
 To view the project open `http://localhost:3000`
 
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const url = require("url");
+const { AsgardeoExpressClient } = require("@asgardeo/auth-express");
 const cookieParser = require("cookie-parser");
 const express = require("express");
 const rateLimit = require("express-rate-limit");
+const config = require("./config.json");
 
 const rateLimiter = rateLimit({
   max: 100,
@@ -18,9 +21,83 @@ app.use(rateLimiter);
 app.set("view engine", "ejs");
 
 app.use("/", express.static("static"));
+app.use("/home", express.static("static"));
+
+AsgardeoExpressClient.getInstance(config);
+
+const onSignIn = (res) => {
+  res.redirect("/home");
+}
+
+const onSignOut = (res) => {
+  res.redirect("/");
+}
+
+const onError = (res, error) => {
+  res.redirect(
+    url.format({
+      pathname: "/",
+      query: {
+        message: error && error.message
+      }
+    })
+  );
+}
+
+app.use(AsgardeoExpressClient.asgardeoExpressAuth(onSignIn, onSignOut, onError));
+
+
+const dataTemplate = {
+  authenticateResponse: null,
+  error: false,
+  errorMessage: "",
+  idToken: null,
+  isAuthenticated: true,
+  isConfigPresent: Boolean(config && config.clientID && config.clientSecret)
+};
 
 app.get("/", async (req, res) => {
-  res.render("landingPage");
+  let data = { ...dataTemplate };
+  data.error = req.query.message ? true : false;
+  data.errorMessage =
+    req.query.message ||
+    "Something went wrong during the authentication process.";
+  res.render("landingPage", data);
+});
+
+const authCallback = (res, error) => {
+  res.redirect(
+    url.format({
+      pathname: "/",
+      query: {
+        message: error
+      }
+    })
+  );
+
+  return true;
+};
+
+const isAuthenticated = AsgardeoExpressClient.protectRoute(authCallback);
+
+app.get("/home", isAuthenticated, async (req, res) => {
+  const data = { ...dataTemplate };
+
+  try {
+    data.idToken = data.isAuthenticated
+      ? await req.asgardeoAuth.getIDToken(req.cookies.ASGARDEO_SESSION_ID)
+      : null;
+
+    data.authenticateResponse = data.isAuthenticated
+      ? await req.asgardeoAuth.getBasicUserInfo(req.cookies.ASGARDEO_SESSION_ID)
+      : {};
+
+    data.error = req.query.error === "true";
+
+    res.render("homePage", data);
+  } catch (error) {
+    res.render("homePage", { ...data, error: true });
+  }
 });
 
 app.listen(PORT, () => {
